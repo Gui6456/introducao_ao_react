6673ce675d8c554ad6084fc1e554d6224c8f1bac
Gui6456 <guiborgia@gmail.com>
2026-09-03 15:06:05 -0300

    aprendizado de variáveis e funções React

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,53 @@
-import { useState } from 'react';
+import { useEffect, useState } from "react"  // Ao usar o useState em algum lugar, automaticamente esse import aparece na primeira vez.
 
-function App() {
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/* useEffect(() => {
+  let intervalo = setInterval(() => {
+    setCliques((cliques) => cliques + 1) // setCliques é a função que altera o valor da variável cliques, e cliques += 1 é o valor que será alterado, ou seja, o valor atual de cliques + 1, a função atualiza a variável.
+    }, 100) // 100 ms = 0,1 seg
+    
+    return () => clearInterval(intervalo) // O return é uma função que é executada quando o componente é desmontado, ou seja, quando a tela é atualizada, e o clearInterval(intervalo) é a função que limpa o intervalo, ou seja, para de executar a função setCliques.
+    }, []) */    // Apenas uma curiosidade de como clicar sozinho.
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-  let nome = "Senac"
-  let sobrenome = "São Carlos"
-  let nome_completo = nome + " " + sobrenome
-///////////////////////////////////////////////////////////////////
-  // useState é uma forma de criar variáveis que podem ser alteradas e que quando alteradas, o React atualiza a tela automaticamente, sem precisar atualizar a página inteira.
-  let [cliques, setCliques] = useState(0) 
+  // Tudo deve ser colocado dentro da função App, pois o React só renderiza o que está dentro dela, mas as variáveis e funções podem colocadas fora dela, porém não aparecem na tela.
+  function App() {  
+    
+  // useState cria um estado no componente. Quando o estado é alterado através da função setter (setNome, setCliques etc.), o React renderiza novamente o componente para atualizar a tela.
+  let [cliques, setCliques] = useState(0)
 
   function soma_cliques() {
-    setCliques(cliques + 1)
+    setCliques(cliques + 1) // setCliques é a função que altera o valor da variável cliques, e cliques += 1 é o valor que será alterado, ou seja, o valor atual de cliques + 1, a função atualiza a variável.
+  }
+  
+  let [nome, setNome] = useState("Carregando...")
+  
+  function carregar_nome(){
+    setNome("Guilherme")     
   }
 
-  return (   // Tudo dentro de {} será tratato como JS puro, ou seja, código, e tudo fora de {} será tratado como HTML puro 
+  return (   
     <div>
       <h1>Hello {2+2} World!!!!!!!</h1>
-      <p>Estou aprendendo React xD</p>  {/*{console.log("qlocura"), é possivel até mesmo colocar códigos {} entre as palavras*/}
-      {/*Esse é um jeito novo no React de mostrar variáveis, muito mais simples */}
-      <p>Eu estudo no {nome} na unidade de {sobrenome}, portando estudo no {nome_completo}</p>  
+      <p>Estou aprendendo React xD</p>
+      <p>Meu nome é {nome}</p>
+      <button onClick={() => setNome("Guilherme")}> Carregar nome </button>  
+      {/* Ou fazer assim, <button> onClick={carregar_nome} */}
+      {/* Dessa forma eu preciso criar uma função para modificar uma variável(global ou local), acho que a () => é mais direta */} 
 
       <hr/>
 
       <p>Você clicou {cliques} vezes</p>
-      <button onClick={soma_cliques}> Clique Aqui</button>
+      <button onClick={soma_cliques}> Clique Aqui</button> 
+      {/*
+            Arrow Function -> Função do tipo seta (lambda) () =>
+            Função anônima -> Função sem nome, que não precisa ser declarada antes de ser usada, e é usada apenas uma vez.
+      
+      */}
+
+      <hr/>
     </div>    
-  )
+  ) 
 }
 
 export default App
